323d16b7ea95e415b83c532c4fdbf43bc681c175
handikatriarlan <handikaarlan@gmail.com>
2025-02-24 21:39:51 +0700

    feat(auth): implement login functionality

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router();
 const registerController = require('../controllers/RegisterController');
-const { validateRegister } = require('../utils/validators/auth');
+const loginController = require('../controllers/LoginController');
+const { validateRegister, validateLogin } = require('../utils/validators/auth');
 
 router.post('/register', validateRegister, registerController.register);
+router.post('/login', validateLogin, loginController.login);
 
 module.exports = router
