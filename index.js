69afc60a2395edb2c62460e04cfd6b99abbd42c4
handikatriarlan <handikaarlan@gmail.com>
2025-02-24 21:33:02 +0700

    feat(auth): implement user registration endpoint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const router = require('./routes')
 const app = express()
 
 app.use(cors())
@@ -12,6 +13,8 @@ const port = 3000
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use('/api', router);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
