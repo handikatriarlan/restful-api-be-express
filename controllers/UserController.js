ff0f27751edddff8db2b0a0a11709ae1f07c258a
handikatriarlan <handikaarlan@gmail.com>
2025-02-24 21:52:06 +0700

    feat(user): add create user endpoint

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,6 @@
 const prisma = require("../prisma/client");
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 const findUsers = async (req, res) => {
     try {
@@ -27,4 +29,40 @@ const findUsers = async (req, res) => {
     }
 };
 
-module.exports = { findUsers };
+const createUser = async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            success: false,
+            message: "Validation error",
+            errors: errors.array(),
+        });
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    try {
+        const user = await prisma.user.create({
+            data: {
+                name: req.body.name,
+                email: req.body.email,
+                password: hashedPassword,
+            },
+        });
+
+        res.status(201).send({
+            success: true,
+            message: "User created successfully",
+            data: user,
+        });
+
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "Internal server error",
+        });
+    }
+};
+
+module.exports = { findUsers, createUser };
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const registerController = require('../controllers/RegisterController');
 const loginController = require('../controllers/LoginController');
 const userController = require('../controllers/UserController');
 const { validateRegister, validateLogin } = require('../utils/validators/auth');
+const { validateUser } = require('../utils/validators/user');
 const verifyToken = require('../middlewares/auth');
 
 router.post('/register', validateRegister, registerController.register);
 router.post('/login', validateLogin, loginController.login);
 router.get('/admin/users', verifyToken, userController.findUsers);
+router.post('/admin/users', verifyToken, validateUser, userController.createUser);
 
 module.exports = router
