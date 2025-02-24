4748bf8328273a819deed6893713cf28116e83b0
handikatriarlan <handikaarlan@gmail.com>
2025-02-24 21:47:53 +0700

    feat(user): add get all users controller and route

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const router = express.Router();
 const registerController = require('../controllers/RegisterController');
 const loginController = require('../controllers/LoginController');
+const userController = require('../controllers/UserController');
 const { validateRegister, validateLogin } = require('../utils/validators/auth');
+const verifyToken = require('../middlewares/auth');
 
 router.post('/register', validateRegister, registerController.register);
 router.post('/login', validateLogin, loginController.login);
+router.get('/admin/users', verifyToken, userController.findUsers);
 
 module.exports = router
